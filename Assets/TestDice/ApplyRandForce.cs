0c6ff49f8e3675cc84f2bf92254f96d1f9e829c9
Haaruk <36482483@users.noreply.github.com>
2018-02-14 17:01:07 -0500

    First implement of dices
    Dice v0.1

---
+++ Assets/TestDice/ApplyRandForce.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyRandForce : MonoBehaviour {
+
+    public string buttonName = "Fire1";
+    public float forceAmount = 10.0f;
+    public float torqueAmount = 10.0f;
+    public ForceMode forceMode;
+    
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetButtonDown(buttonName))
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.AddForce(Random.onUnitSphere*forceAmount,forceMode); 
+            rb.AddTorque(Random.onUnitSphere * torqueAmount, forceMode);
+        }
+	}
+}
---
+++ Assets/TestDice/DisplayCurrentDieValue.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayCurrentDieValue : MonoBehaviour
+{
+    public LayerMask dieValueColliderLayer = -1;
+
+    private int currentValue = 1;
+
+    private bool rollComplete = false;
+
+    // Update is called once per frame
+    void Update () {
+
+        //ADD DiceMap for vector3 values of all faces (for all dices) in an external file, in XML-like easy access structure
+        //ADD Refactor...
+
+        // Update only when dice is stopped
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        if (rb.IsSleeping() && !rollComplete)
+        {
+            rollComplete = true;
+            Debug.Log("Dice stopped rolling, result is: " + currentValue.ToString());
+        }
+        else if(!rb.IsSleeping())
+        {
+            rollComplete = false;
+        }
+
+
+        // Finding which face is pointing up
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position,Vector3.up,out hit,Mathf.Infinity,dieValueColliderLayer))
+        {
+            // Reading the value of the collider on the die top face
+            currentValue = hit.collider.GetComponent<DieValue>().value;
+        }
+    }
+
+    void OnGUI()
+        {
+            GUILayout.Label(currentValue.ToString());
+        }
+    
+}
